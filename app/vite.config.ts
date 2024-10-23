ac31bdfdbf0dfe5e3da535095eb8d37f39e343d8
GautamSharda <57648023+GautamSharda@users.noreply.github.com>
2024-10-23 15:07:29 -0500

    runs with npm run tauri dev

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,32 +1,32 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
-// https://vitejs.dev/config/
-export default defineConfig(async () => ({
-  plugins: [react()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
+export default defineConfig({
+  // prevent vite from obscuring rust errors
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 1420,
+    // Tauri expects a fixed port, fail if that port is not available
     strictPort: true,
+    // if the host Tauri is expecting is set, use it
     host: host || false,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
-      : undefined,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
-    },
+    port: 5173,
   },
-}));
+  // Env variables starting with the item of `envPrefix` will be exposed in tauri's source code through `import.meta.env`.
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target:
+      // @ts-expect-error process is a nodejs global
+      process.env.TAURI_ENV_PLATFORM == 'windows'
+        ? 'chrome105'
+        : 'safari13',
+    // don't minify for debug builds
+    // @ts-expect-error process is a nodejs global
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    // @ts-expect-error process is a nodejs global
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
+});
