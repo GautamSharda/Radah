f8651ac85801ae50fbb1ed5a44ab68dea6ffbc14
GautamSharda <57648023+GautamSharda@users.noreply.github.com>
2024-10-24 23:38:58 -0500

    works

---
+++ app/src/components/VNCView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VNCView: React.FC = () => {
+  return (
+    <div>
+      <h2>VNC View</h2>
+      <p>This is a placeholder for the VNC view component.</p>
+    </div>
+  );
+};
+
+export default VNCView;
