eb9463e214519a3f40e3606d84d6bca364559109
paaatrrrick <patrick.123.foster@gmail.com>
2024-11-06 12:48:08 -0800

    hotfix

--- app/src/components/view-agent/MessageInput.tsx
+++ app/src/components/view-agent/MessageInput.tsx
@@ -48,7 +48,7 @@ export function MessageInput({ sendMessage, promptRunning, currentAgentID, stopA
             />
             <div className="flex flex-row items-center justify-end w-full">
                 <div className="w-8 h-8 bg-slate-800 rounded-full flex items-center justify-center hover:cursor-pointer hover:bg-slate-700">
-                    {promptRunning === "false" && <ArrowUpIcon className="h-5 w-5 stroke-[1.5] text-white" />}
+                    {promptRunning === "false" && <ArrowUpIcon className="h-5 w-5 stroke-[1.5] text-white" onClick={sendMessageWrapper} />}
                 </div>
                 {/* {promptRunning === "false" && <ArrowUpIcon className="mr-2 h-4 w-4 bg-red-400" width={100} height={100} />} */}
                 {/* {promptRunning === "true" && <Button type="submit" className='ml-3 font-mono w-24 bg-red-400 text-white h-12' onClick={stopAgent}>Stop</Button>}
